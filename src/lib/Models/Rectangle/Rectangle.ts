94a9ab2fe414f22ccc368dbbca09bf0aef155a72
Evan Garrett <evan@fitfox.de>
2019-08-20 22:09:59 +0200

    First Commit -- Parcel setup

---
+++ src/lib/Models/Rectangle/Rectangle.ts
@@ -0,0 +1,10 @@
+class Rectangle {
+    public constructor(
+        public x: number,
+        public y: number,
+        public xOffset: number,
+        public yOffset: number,
+    ) {}
+}
+
+export default Rectangle;
---
+++ src/lib/Models/Vector/Vector.ts
@@ -0,0 +1,8 @@
+class Vector {
+    public constructor(
+        public x: number,
+        public y: number
+    ) {}
+}
+
+export default Vector;
