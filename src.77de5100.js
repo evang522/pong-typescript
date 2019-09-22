81eefe82e587f1fe2d893672ab46608a7bcd5ea8
Evan Garrett <evang522@gmail.com>
2019-09-22 21:20:56 +0200

    Updates

--- src.77de5100.js
+++ src.77de5100.js
@@ -796,7 +796,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42891" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41203" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
